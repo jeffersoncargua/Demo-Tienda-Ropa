a6c33a951d741f27738f55010b136bf745f89e23
Jefferson Cargua <jeffersoncargua@gmail.com>
2026-01-13 11:26:15 -0500

    Se mejoro el diseño responsivo de la pagina de cart en los botones

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -127,37 +127,44 @@ export const Cart = () => {
 				<div className="flex flex-row items-center justify-center md:justify-end space-x-4 text-xs md:text-sm">
 					<Link
 						to={"/order"}
-						className="bg-green-600 hover:bg-green-700 hover:text-white rounded-lg flex flex-row justify-center items-center px-5 py-2  hover:border-2 hover:border-black font-semibold"
+						className="bg-green-600 hover:bg-green-700 hover:text-white rounded-lg flex flex-col sm:flex-row justify-center items-center px-5 py-2  hover:border-2 hover:border-black font-semibold"
 					>
-						Ir a Pagar: ${total.toFixed(2)}
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							fill="currentColor"
-							className="bi bi-cash-coin w-5 h-5 ms-2.5"
-							viewBox="0 0 16 16"
-						>
-							<path
-								fillRule="evenodd"
-								d="M11 15a4 4 0 1 0 0-8 4 4 0 0 0 0 8m5-4a5 5 0 1 1-10 0 5 5 0 0 1 10 0"
-							/>
-							<path d="M9.438 11.944c.047.596.518 1.06 1.363 1.116v.44h.375v-.443c.875-.061 1.386-.529 1.386-1.207 0-.618-.39-.936-1.09-1.1l-.296-.07v-1.2c.376.043.614.248.671.532h.658c-.047-.575-.54-1.024-1.329-1.073V8.5h-.375v.45c-.747.073-1.255.522-1.255 1.158 0 .562.378.92 1.007 1.066l.248.061v1.272c-.384-.058-.639-.27-.696-.563h-.668zm1.36-1.354c-.369-.085-.569-.26-.569-.522 0-.294.216-.514.572-.578v1.1zm.432.746c.449.104.655.272.655.569 0 .339-.257.571-.709.614v-1.195z" />
-							<path d="M1 0a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h4.083q.088-.517.258-1H3a2 2 0 0 0-2-2V3a2 2 0 0 0 2-2h10a2 2 0 0 0 2 2v3.528c.38.34.717.728 1 1.154V1a1 1 0 0 0-1-1z" />
-							<path d="M9.998 5.083 10 5a2 2 0 1 0-3.132 1.65 6 6 0 0 1 3.13-1.567" />
-						</svg>
+						Ir a Pagar: &nbsp;
+						<span className="flex flex-row items-center mt-2 sm:mt-0">
+							${total.toFixed(2)}
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								fill="currentColor"
+								className="bi bi-cash-coin w-5 h-5 ms-2.5"
+								viewBox="0 0 16 16"
+							>
+								<path
+									fillRule="evenodd"
+									d="M11 15a4 4 0 1 0 0-8 4 4 0 0 0 0 8m5-4a5 5 0 1 1-10 0 5 5 0 0 1 10 0"
+								/>
+								<path d="M9.438 11.944c.047.596.518 1.06 1.363 1.116v.44h.375v-.443c.875-.061 1.386-.529 1.386-1.207 0-.618-.39-.936-1.09-1.1l-.296-.07v-1.2c.376.043.614.248.671.532h.658c-.047-.575-.54-1.024-1.329-1.073V8.5h-.375v.45c-.747.073-1.255.522-1.255 1.158 0 .562.378.92 1.007 1.066l.248.061v1.272c-.384-.058-.639-.27-.696-.563h-.668zm1.36-1.354c-.369-.085-.569-.26-.569-.522 0-.294.216-.514.572-.578v1.1zm.432.746c.449.104.655.272.655.569 0 .339-.257.571-.709.614v-1.195z" />
+								<path d="M1 0a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h4.083q.088-.517.258-1H3a2 2 0 0 0-2-2V3a2 2 0 0 0 2-2h10a2 2 0 0 0 2 2v3.528c.38.34.717.728 1 1.154V1a1 1 0 0 0-1-1z" />
+								<path d="M9.998 5.083 10 5a2 2 0 1 0-3.132 1.65 6 6 0 0 1 3.13-1.567" />
+							</svg>
+						</span>
 					</Link>
 					<Link
 						to={"/"}
-						className="bg-yellow-300 hover:bg-yellow-600 hover:text-white rounded-lg flex flex-row justify-center items-center px-5 py-2  hover:border-2 hover:border-black font-semibold"
+						className="bg-yellow-300 hover:bg-yellow-600 hover:text-white rounded-lg flex flex-col sm:flex-row justify-center items-center px-5 py-2  hover:border-2 hover:border-black font-semibold"
 					>
-						Seguir Comprando
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							fill="currentColor"
-							className="bi bi-cart-fill w-5 h-5 ms-2"
-							viewBox="0 0 16 16"
-						>
-							<path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2" />
-						</svg>
+						Seguir &nbsp;
+						<span className="flex flex-row items-center mt-2 sm:mt-0">
+							Comprando
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								fill="currentColor"
+								className="bi bi-cart-fill w-5 h-5 ms-2"
+								viewBox="0 0 16 16"
+							>
+								<path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2" />
+							</svg>
+						</span>
+						
 					</Link>
 				</div>
 			)}
